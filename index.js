16449f96f6ac23bd95bc2dd8b15b6ed3f58bebe9
Ibnu Habibie <ibnu@maven.co.id>
2021-06-30 23:01:38 +0700

    add static file

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var PORT = process.env.PORT || 5000;
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send('Hello World !')
+    res.send('Hello World')
 })
 
 app.listen(PORT, () => {
